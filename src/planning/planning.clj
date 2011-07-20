0038ba1be38a3d155d55f1c5226dabc1c87eed3a
Rino Jose <r.jose@lakeway-tech.com>
2011-07-20 00:25:56 -0700

    Generalized add-member Added some notes

--- src/planning/planning.clj
+++ src/planning/planning.clj
@@ -1,4 +1,5 @@
-(ns planning.planning)
+(ns planning.planning
+  (:use clojure.set))
 
 ; TODO: Use a ref for Person and Team
 ; TODO: Define a function for creating a Person and Team
@@ -8,11 +9,12 @@
 (defrecord Team [name members])
 
 
-(defn add-member
-  "Adds a new member to a team"
-  [team person]
-  (Team. (:name team) (conj (:members team) person)))
-
+; TODO: Make members a set
+(defn add-members
+  [team & new-members]
+  (let [members (union (:members team) (set new-members))]
+    (Team. (:name team) members)))
+  
 (defn remove-member
   "Removes a member from a team"
   [{:keys [members] :as team} {:keys [id]}]
@@ -26,19 +28,24 @@
   (let [roles (map #(first (:roles %)) members)]
     (reduce (fn [m role]
               (let [role-count (m role)]
-                (if role-count
-                  (assoc m role (inc role-count))
-                  (assoc m role 1))))
+                (if (nil? role-count)
+                  (assoc m role 1)
+                  (assoc m role (inc role-count)))))
             {} roles)))
-   
-  
+
+; TODO: Add a function to get all the roles for a team
+; TODO: Add a function that can add projects
+; TODO: Add a function that can determine resource availability. We should do one version greedily
+; Another version might try to be smarter about how to allocate someone who can do multiple things
+; We should have the sense of project priority
+; We should be able to estimate project overload to hold dates
+; We should be able to compute project breakpoints to preserve resource loading
 ; TODO: Write tests for primary roles
 ;       Include cases where there are no roles for someone
 
-(def a-team (Team. "SW Team" []))
+(def a-team (Team. "SW Team" #{}))
 (def rino (Person. 100 "Rino Jose" ["Node Engineer" "Rails Engineer" "SW Manager"]))
 (def roland (Person. 101 "Roland Jose" ["Warblade Knight"]))
 (def james (Person. 102 "James Simonsen" ["Node Engineer"]))
 
-; TODO: Create a way to add multiple members to a team at once
-(def new-team (add-member (add-member a-team rino) james))
+(def new-team (add-members a-team rino roland))
